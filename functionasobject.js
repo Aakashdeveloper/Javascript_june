a8be868c0c159dd07194abdc2654871b0c236761
AAKASH HANDA <AAKASH HANDA>
2017-07-08 07:38:14 +0530

    a

---
+++ functionasobject.js
@@ -0,0 +1,17 @@
+function person(name,age){
+	this.name=name;
+	this.age = age;
+	this.subject= "javascript"
+
+}
+
+var even = new person("even1",20)
+even.skincolor= "fair"
+even.subject="angular"
+
+
+var susan = new person()
+var susan = new person("susan1",30)
+
+susan
+person {name: "susan1", age: 30}
